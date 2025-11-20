f2ebf917d3e40c90e90c63b419a973711768fc4d
Alice Vital <alice.nascimento@bsatech.io>
2025-11-20 17:29:06 -0300

    refactor: modifica models e adiciona campos neles.

--- app/models.py
+++ app/models.py
@@ -3,9 +3,20 @@ from pydantic import BaseModel
 
 class User(BaseModel):
     id: int
-    name: str
+    username: str
     email: str
     description: str
+    password: str
+
+class CreateUser(User):
+    useername: str
+    email: str
+    password: str
+
+class UserLogin(User):
+    username: str
+    email: str
+    password: str
 
 class Task(BaseModel):
     id: int
@@ -14,3 +25,7 @@ class Task(BaseModel):
     status: str
     content: str
 
+class CreateTask(Task):
+    name: str
+    status: str
+    content: str
--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -1,35 +1,3 @@
 '''
 Nesse arquivo ficará as rotas das tarefas (Listar, Criar, Atualizar e Deletar).
 '''
-from fastapi import APIRouter, Depends, HTTPException
-
-from pydantic import BaseModel
-
-router = APIRouter(
-    prefix='/tasks',
-    tags=['Tasks']
-)
-class PostRegister(BaseModel):
-    title: str
-    description: str
-    author: str
-
-router.post('/tasks/')
-def write_tasks():
-    pass
-
-router.get('/tasks/')
-def show_tasks():
-    pass
-
-router.get('/tasks/{id}')
-def show_id_tasks():
-    pass
-
-router.put('/tasks/{id}')
-def update_tasks():
-    pass
-
-router.delete('/tasks/{id}')
-def delete_tasks():
-    pass
