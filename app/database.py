ee03a37046abe33ef96929c65e34b9e324811d63
Alice Emily <alice.nascimento@bsatech.io>
2025-11-29 16:10:09 -0300

    feat: adiciona conexão com mongodb

--- app/database.py
+++ app/database.py
@@ -1,3 +1,10 @@
 '''
 Nesse arquivo ficará a conexão com o banco de dados.
 '''
+from motor.motor_asyncio import AsyncIOMotorClient
+
+MONGO_URL = "mongodb://localhost:27017"
+cliente = AsyncIOMotorClient(MONGO_URL)
+
+database = cliente.fastapi
+
