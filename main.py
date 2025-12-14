4b0eeba1760ecf2629819a8f23a0f7001c05ff2f
Alice Emily <alice.nascimento@bsatech.io>
2025-12-14 02:21:26 -0300

    feat: add include_router in main

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 
-from app.routers import auth, users, tasks # Importa os arquivos de rotas
+from app.routers import auth_router, task_router, users_router # Importa os arquivos de rotas
 
 app = FastAPI()
 
-app.include_router(auth.router) #registra as rotas de cada um dos arquivos
-app.include_router(users.router) # o include_router faz o fastapi reconhecer as rotas que estão dentro dos arquivos
-app.include_router(tasks.router)
+app.include_router(auth_router.router) #registra as rotas de cada um dos arquivos
+app.include_router(users_router.router) # o include_router faz o fastapi reconhecer as rotas que estão dentro dos arquivos
+app.include_router(task_router.router)
 
 @app.get('/')
 def root():
